311f6195e062294df94bed550be97122b5bfd69c
Seunggyu <seunggyul14@icloud.com>
2023-01-23 12:07:27 -0800

    tweek exit .exit q git add .! git commit -m tweek

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ActiveRecord::Base
     has_many :posts
     has_many :comments, through: :posts
-
-    def self.login(username, password)
-        self.all.find_by(username: username, password: password)
-    end
 end
