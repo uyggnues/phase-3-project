6299524c0aca77fd4c181bf87639a4c6821fbadc
Seunggyu <seunggyul14@icloud.com>
2023-01-23 14:47:20 -0800

    comment

--- project/app/models/post.rb
+++ project/app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
     has_many :comments, dependent: :destroy
     belongs_to :user
-    # validates :username, :password 
+    validates :username, :password, presence: true
 end
