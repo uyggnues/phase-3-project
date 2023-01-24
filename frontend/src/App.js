20bddf64b36aa7feb1972b4a896a35b0751bc12d
Andrew <aforest41@yahoo.com>
2023-01-24 08:03:24 -0800

    pulling from seunggyu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import {useEffect} from 'react'
+import Post from './Components/Post'
+import { useState, useEffect } from 'react';
 
 function App() {
-  
+  const [post, setPost] = useState({});
+
   useEffect(() => {
-    fetch("http://127.0.0.1:9393/posts")
+    fetch("http://localhost:3000/posts")
     .then(resp => resp.json())
-    .then(data => {debugger})
-  },[])
-
-
+    .then(data => console.log(data))//setPost(data))
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      {/* <Post post={post}/> */}
     </div>
   );
 }
