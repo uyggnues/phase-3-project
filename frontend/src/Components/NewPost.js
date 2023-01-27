d4989e3e0d409fa47d6c3ea8507b74cef9c7b39d
Andrew <aforest41@yahoo.com>
2023-01-26 16:46:10 -0800

    minor changes

--- frontend/src/Components/NewPost.js
+++ frontend/src/Components/NewPost.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
+import validator from 'validator'
 
 function NewPost({user, setPosts}){
     const navigate = useNavigate()
