0b544640706b8e52466c8389a9f76177fed60291
Seunggyu <seunggyul14@icloud.com>
2023-01-30 09:00:39 -0800

    sign up

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -9,4 +9,6 @@ class User < ActiveRecord::Base
     validates :username, presence: true, length: {in: 4..25}
     validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
     validates :password, length: {in: 6..25}
+    # validates :comments, presence: true
+    # validates :posts, presence: true
 end
